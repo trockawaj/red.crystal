5f804a7078ac8b2c400b8f70d4043b60e9116346
Yousuke SakiyamaYousuke Sakiyama <skym.yousuke@gmail.com>
2026-02-16 18:18:04 +0900

    Round 45: Fix build error - Add missing Link import in Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Hero from '../components/Hero';
 import About from '../components/About';
 import Gallery from '../components/Gallery';
